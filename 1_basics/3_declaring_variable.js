15d84c37fafd727a88a65f3df6f501db2127f2f4
ByeoungJoon Jeon <juni8453@naver.com>
2023-12-12 20:13:20 +0900

    1 ~ 3강

---
+++ 1_basics/3_declaring_variable.js
@@ -0,0 +1,28 @@
+/**
+ * JS 에서 변수 선언
+ * 
+ * 1) var - 이제 안씀 let, const 로 모두 대체
+ * 2) let - 선언 한 뒤 값을 추후 변경할 수 있다. (가변)
+ * 3) const - 선언 한 뒤 값을 추후 변경할 수 없다. (불변 / final 과 비슷)
+ */
+
+var name = 'code factory';
+console.log(name);
+
+var age = 32;
+console.log(age);
+
+let ive = '아이브';
+console.log(ive)
+
+ive = '안유진';
+console.log(ive);
+
+const newJeans = '뉴진스';
+console.log(newJeans);
+
+var undifiend_var;
+console.log(undifiend);
+
+let name;
+console.log(name);
